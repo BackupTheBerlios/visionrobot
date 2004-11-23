2a493ac246560649a14dfc35b9a07bda536e6987
zosco <zosco>
2004-11-23 13:01:52 +0000

    Imprime algunas cosas más.

--- antiguo/plugin_nulo_gtk/src/main.c
+++ antiguo/plugin_nulo_gtk/src/main.c
@@ -8,36 +8,36 @@ char buffer[128];
 
 int ciclo()
 {
-	printf("Ciclo con datos \"%s\".\n", buffer);
+	printf("ciclo con datos \"%s\".\n", buffer);
 	return 0;
 }
 
 int set_datos(const void * datos) {
-	printf("Set_Datos: %s.\n", (char *) datos);
+	printf("set_datos recibe: %s.\n", (char *) datos);
 	strcpy(buffer, datos);
 	return 0;
 
 }
 
 void * get_datos() {
-	printf("Get_Datos.\n");
+	printf("get_datos devuelve: %s.\n", buffer);
 	return buffer;
 }
 
 int iniciar() {
-	printf("Iniciar.\n");
+	printf("iniciar.\n");
 	return 0;
 
 }
 
 int propiedades() {
-	printf("Propiedades.\n");
+	printf("propiedades.\n");
 	return 0;
 
 }
 
 int cerrar(){
-	printf("Cerrar.\n");
+	printf("cerrar.\n");
 	return 0;
 
 }
