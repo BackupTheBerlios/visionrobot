9f7e19d06f508cc6de3023da55a0c33832b19098
zosco <zosco>
2005-03-02 12:41:34 +0000

    - El filtro de imágenes ahora carga archivo también

--- antiguo/vision_15_02_05/imagenes.c
+++ antiguo/vision_15_02_05/imagenes.c
@@ -10,7 +10,7 @@
 #include "ventana_imagen_sdk.h"
 #include <stdlib.h>
 #include <string.h>
-
+#include <gdk-pixbuf/gdk-pixbuf.h>
 
 typedef struct {
   filtro_gestos_in_t m_filtro;
@@ -18,7 +18,8 @@ typedef struct {
   unsigned char m_rojo;
   unsigned char m_verde;
   unsigned char m_azul;
-  char m_aleatorio;
+  GdkPixbuf *m_imagen_archivo;
+  enum {IMAGEN, ALEATORIO, COLOR} m_tipo;
 } dato_imagenes_t;
 
 
@@ -26,23 +27,40 @@ static void imagenes_generar_imagen(modulo_t *modulo) {
   int i, j;
   dato_imagenes_t *dato = (dato_imagenes_t*)modulo->m_dato;
   filtro_gestos_in_t* imagen = (filtro_gestos_in_t*)&dato->m_filtro;
+  int ancho_for = imagen->m_dato.imagen.m_ancho * imagen->m_dato.imagen.m_bytes;
+  guchar * p;
+  guchar *aux;
+  guchar * b;
 
-  if(dato->m_aleatorio) {
+  switch(dato->m_tipo) {
+  case IMAGEN:
+    p = gdk_pixbuf_get_pixels (dato->m_imagen_archivo);
+    b = imagen->m_dato.imagen.m_imagen;
+    for(j = 0; j < imagen->m_dato.imagen.m_alto; j++) {
+      aux = &p[(j + 1) * ancho_for];
+      for(i = 0; i < ancho_for; i += 1) {
+	*b++ = *--aux;
+	//	*--aux = *b++;	
+      }
+    }    
+    break;
+  case ALEATORIO:
     for(i = 0; i < imagen->m_dato.imagen.m_alto; i++) {
-      for(j = 0; j < imagen->m_dato.imagen.m_ancho * imagen->m_dato.imagen.m_bytes; j += 1) {
+      for(j = 0; j < ancho_for; j += 1) {
 	imagen->m_dato.imagen.m_imagen[i * imagen->m_dato.imagen.m_ancho * imagen->m_dato.imagen.m_bytes + j] =
 	  (int)(g_random_double() * 255);
       }
     }
-  }
-  else {
+    break;
+  case COLOR:
     for(i = 0; i < imagen->m_dato.imagen.m_alto; i++) {
-      for(j = 0; j < imagen->m_dato.imagen.m_ancho * imagen->m_dato.imagen.m_bytes; j += 3) {
+      for(j = 0; j < ancho_for; j += 3) {
 	imagen->m_dato.imagen.m_imagen[i * imagen->m_dato.imagen.m_ancho * imagen->m_dato.imagen.m_bytes + j] = dato->m_azul;
 	imagen->m_dato.imagen.m_imagen[i * imagen->m_dato.imagen.m_ancho * imagen->m_dato.imagen.m_bytes + j + 1] = dato->m_verde;
 	imagen->m_dato.imagen.m_imagen[i * imagen->m_dato.imagen.m_ancho * imagen->m_dato.imagen.m_bytes + j + 2] = dato->m_rojo;
       }
     }
+    break;
   }
 }
 
@@ -54,21 +72,27 @@ static char *imagenes_ciclo(modulo_t* modulo, char tipo, GHashTable *lista)
 
 static char *imagenes_iniciar(modulo_t* modulo, GHashTable *argumentos)
 {
-  if(g_hash_table_size(argumentos) < 3) return "faltan parametros";
   dato_imagenes_t *dato = (dato_imagenes_t*)modulo->m_dato;
   filtro_gestos_in_t* imagen = &dato->m_filtro;
-
+  dato->m_imagen_archivo = 0;
   char * rojo = g_hash_table_lookup(argumentos,"rojo");
   char * verde = g_hash_table_lookup(argumentos,"verde");
   char * azul = g_hash_table_lookup(argumentos,"azul");
-  if(rojo && verde && azul) {
-    dato->m_rojo = (unsigned char)atoi(rojo);
-    dato->m_verde = (unsigned char)atoi(verde);
-    dato->m_azul = (unsigned char)atoi(azul);
-    dato->m_aleatorio = 0;
+  char * archivo_imagen = g_hash_table_lookup(argumentos,"archivo");
+  if(archivo_imagen) {
+    dato->m_tipo = IMAGEN;
+    dato->m_imagen_archivo = gdk_pixbuf_new_from_file(archivo_imagen, 0);    
   }
-  else {
-    dato->m_aleatorio = 1;
+  if(!dato->m_imagen_archivo) {
+    if(rojo && verde && azul) {
+      dato->m_rojo = (unsigned char)atoi(rojo);
+      dato->m_verde = (unsigned char)atoi(verde);
+      dato->m_azul = (unsigned char)atoi(azul);
+      dato->m_tipo = COLOR;
+    }
+    else {
+      dato->m_tipo = ALEATORIO;
+    }
   }
     
   imagen->m_tipo = PIPELINE_FILTRO_GESTOS_IMAGEN;
@@ -97,7 +121,9 @@ static char *imagenes_cerrar(modulo_t* modulo)
   dato_imagenes_t *dato = (dato_imagenes_t*)modulo->m_dato;
   filtro_gestos_in_t* imagen = &dato->m_filtro;
   free(imagen->m_dato.imagen.m_imagen);
-  //  free(imagen);
+  if(dato->m_imagen_archivo) {
+    gdk_pixbuf_unref(dato->m_imagen_archivo);
+  }
   free(dato);
   free(modulo);
   return "cerrado";
@@ -112,6 +138,5 @@ modulo_t * get_modulo()
   modulo->m_cerrar = imagenes_cerrar;
   modulo->m_ciclo = imagenes_ciclo;
   modulo->m_dato = (dato_imagenes_t*)malloc(sizeof(dato_imagenes_t));  
-  //  ((dato_imagenes_t*)modulo->m_dato)->m_filtro = (filtro_gestos_in_t*)malloc(sizeof(filtro_gestos_in_t));  
   return modulo;
 }
