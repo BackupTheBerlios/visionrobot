2f473c8cdeb3051d9c3c33d518a077ab9479c070
zosco <zosco>
2005-03-09 16:08:13 +0000

    - DCG empezada - Debería funcionar :S DevIL - También presentamos el módulo Gestion, para filtrar la salida de la red

--- antiguo/vision-1_4_05/dcg.pl
+++ antiguo/vision-1_4_05/dcg.pl
@@ -1,4 +1,53 @@
-frase (E, S) :-
-	S is 24,
-	write(S),
-	nl.
+camaron(E, S) :-
+	frase(E, []).
+
+camaron(E, S) :-
+	expresion(S, E, []).
+
+% Lenguaje natural	
+frase -->
+	gn,
+	gv.
+
+gn -->
+	determinante,
+	nombre.
+
+gv -->
+	verbo,
+	gn.
+
+% Expresiones aritmeticas
+expresion(Z) --> 
+	term(X), [+], expresion(Y), {Z is X + Y}.
+expresion(Z) -->
+	term(X), [-], expresion(Y), {Z is X - Y}.
+expresion(Z) -->
+	term(X).
+
+termino(Z) -->
+	factor(X), [*], termino(Y), {Z is X * Y}.
+termino(Z) -->
+	factor(X), [/], termino(Y), {Z is X / Y}.
+termino(Z) -->
+	factor(Z).
+
+factor(N) -->
+	[+], factor(N).
+factor(N) -->
+	[-], factor(M), {N is - M}.
+factor(N) -->
+	[(], expresion(N), [)].
+factor(N) -->
+	[X].
+
+
+
+% Diccionario
+determinante -->
+	[el].
+nombre -->
+	[panda].
+
+verbo -->
+	[comer].
