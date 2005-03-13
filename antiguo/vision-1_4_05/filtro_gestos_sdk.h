3c4da415634efd457141f7f0cf548c3c884ed870
diegosanchez19 <diegosanchez19>
2005-03-13 20:28:58 +0000

    Introducidas las funciones para el filtrado de carteles

--- antiguo/vision-1_4_05/filtro_gestos_sdk.h
+++ antiguo/vision-1_4_05/filtro_gestos_sdk.h
@@ -1,6 +1,11 @@
 #ifndef _FILTRO_GESTOS_SDK_H_
 #define _FILTRO_GESTOS_SDK_H_
 
+const unsigned char NEGRO= 0;
+const unsigned char BLANCO= 255;
+const unsigned char SEMI_NEGRO= 30;
+const int TOL=1;
+
 typedef unsigned char color_t;
 
 typedef struct {
@@ -18,5 +23,40 @@ typedef struct {
   color_t m_azul_sup; 
   color_t m_azul_inf;
 } filtro_gestos_in_parametros_t;
+
+
+//***********************************************************************************
+//Nuevas estructuras para el filtro de carteles
+
+typedef struct {
+        color_t ru;
+        color_t rd;
+        color_t gu;
+        color_t gd;
+        color_t bu;
+        color_t bd;
+} special_colour_t;
+
+typedef struct {
+        int x;
+        int y;
+} coord_t;
+
+typedef struct {
+        coord_t ii;
+        coord_t si;
+        coord_t id;
+        coord_t sd;
+        int grados;
+}bounds_t;
+
+typedef struct{
+    color_t* dibujo;
+    int ancho;
+    int alto;
+}dato_t;
+
+//*****************************************************************************
+
 
 #endif
